65a3e7b8f0bbdbe29f3f92f52cf2267a04aa096a
mcpeckgroup <eric@mcpeckgroup.com>
2025-09-01 22:34:56 -0400

    Fix: disable prerender (dynamic pages) + ensure envs on Vercel

--- src/app/requests/[id]/page.tsx
+++ src/app/requests/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 'use client';
 import { useEffect, useMemo, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
